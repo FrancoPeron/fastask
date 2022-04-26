d047d1b9485969cc5ac1eafbd8252431a49a9259
Franco Peron <francomatiasperon@gmail.com>
2022-04-25 22:08:11 -0300

    andaa

--- main.js
+++ main.js
@@ -4,20 +4,22 @@ import './style.scss'
 function taskItem(val){
     const item =  
         `   
-        <li id="task${val.id}" class="task-item flex flex-row justify-between align-items-start mb-8">
+        <li class="task-item flex flex-row justify-between align-items-start mb-8">
 
             <div class="flex flex-row align-items-start flex-fill">
                 <input class="checkbox mr-12 mt-4" type="checkbox" ${val.done}>
-                <p task="${val.id}" class="task fw-6 f-b1 c-g1" placeholder="Add a task..." contenteditable="true">${val.content}</p>
-            </div> 
-
-            <svg class="trash cursor-pointer p-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                <p class="task fw-6 f-b1 c-g1" placeholder="Add a task..." contenteditable="true" tabindex="1">${val.content}</p>
+            </div>
+            
+            
+            <svg btn="trash" class="trash cursor-pointer p-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
                 <g transform="translate(-89.25 -860.75)">
                     <line class="trash-a" x2="24" transform="translate(90.5 867)"/>
                     <path class="trash-b" d="M2,0h8a2,2,0,0,1,2,2V5a0,0,0,0,1,0,0H0A0,0,0,0,1,0,5V2A2,2,0,0,1,2,0Z" transform="translate(96.5 862)"/>
                     <path class="trash-b" d="M0,0H18L16,17a3,3,0,0,1-3,3H5a3,3,0,0,1-3-3Z" transform="translate(93.5 867)"/>
                 </g>
             </svg>
+            
 
         </li>
         `
@@ -27,125 +29,101 @@ function taskItem(val){
 const addArray = (e, index) => {
     
     todos[index] = {
-        id: index,
         done: "",
-        content: (e.currentTarget).innerText,
+        content: e.target.innerText,
     }
-    
-    todos.splice(index+1, 0,  {
-        id: index+1,
+   /*  todos.splice(index+1, 0,  {
         done: "",
         content: "",
-    })
-    if ( todos[index+1].content !== todos[index].content ) {
-      console.log("s")  
+    }) */
+
+   /*  if(todos.length > 1){
+        console.log((todos[index]))
+        
+        if((todos[index]) === ""){
+            todos.splice(index+1, 1,  {
+                done: "",
+                content: "",
+            })
+        }
+        else{
+        
+            todos.splice(index+1, 0,  {
+                done: "",
+                content: "",
+            })
+        }
+    }else{
+        
+        todos.splice(index+1, 0,  {
+            done: "",
+            content: "",
+        })
+    } */
+    
+    
+    
+
+
+}
+
+function deleteArray(){
+    todos.splice(1, 1);
+    printTodos()
+}
+
+
+function click(e){
+    
+
+    let trash = (e.target.parentElement.children)[1].getAttribute("btn") 
+
+    if (trash === "trash" && todos.length > 1) {
+        deleteArray()
     }
 
-
+    console.log(todos);
 }
 
-function showTask(){
-
-
-    cant = 1;
+function keyup(e){
+    e.preventDefault()
     
-        
-    const observer = new MutationObserver((mutationsList) => {
-        // console.log(mutationsList.length)
-        
-        mutationsList.forEach(function(mutation) {
-            
-            console.log((mutation.target));
-            
-            (mutation.target).addEventListener('keydown', (e) => {
-                //console.log(e.target)
-                console.log(e)
-                addArray(e,cant)
-                
-                if (e.key == "Enter"){
-                    
-                    e.preventDefault()
-                    console.log(e.target)
-                    console.log(mutation.addedNodes)
-                    
-                    
-                    const node = document.createElement("li")
-                    node.innerHTML = taskItem(todos[cant])
-                    container.append(node)
-                    e.preventDefault()
-                    cant+=1;
-                    
-                    
-                    
-                } 
+    let newArray =  Array.from(container.children)
+    const index = newArray.indexOf(e.target.parentElement.parentElement);
+    console.log(index,"--");
 
-                
-            });
-
-            (mutation.target).addEventListener('click', (e) => {
-                console.log(e.target)
-            })
-        }); 
-
-    })
-    observer.observe(container, config)
     
+    if (index != -1) {
 
-}
-
-function showTask2(){
-
-    container.insertAdjacentHTML('beforeend',taskItem(todos[cant]));
-    cant+=1;
-    let items = document.querySelectorAll(".task");
-    var el = document.documentElement;
-    el.addEventListener('DOMNodeInserted', e => {
-
-        items.forEach((item,i) => {
-
-
-            item.addEventListener('change', fp)
-
-            // item.removeEventListener('keydown', enter)
-
-            item.addEventListener('keypress', enter)
-            
-        })
-
-        items = document.querySelectorAll(".task")
-
-    }, false);
-        
-}
-
-function enter(e){
-    
-    if (e.key == "Enter"){
-        e.preventDefault()
-
-        let index = parseInt((e.target).getAttribute("task"))
-        console.log(e.target)
-        
         addArray(e,index)
         
-        container.insertAdjacentHTML('beforeend',taskItem(todos[index+1]));
+        if (e.key == "Enter"){
 
-
-        cant+=1;
-
-        // container.insertAdjacentHTML('beforeend',taskItem(todos[cant+1]));
+            todos.splice(index+1, 0,  {
+                done: "",
+                content: "",
+            })
         
-        console.log(todos);
+            printTodos()
+            
+            let todosHtmlArray = Array.from(container.children)
+            let nextNode = todosHtmlArray[index+1].querySelector(".task")
+            setCursor(nextNode)
+            
+        }
 
+        if (e.key === "Backspace" && e.target.innerText == ""){
+            console.log(e.key)
+            deleteArray()
+            
+        }
+        
     }
+
+    console.log(todos);
 }
 
-function fp(e){
-    console.log(2,e)
-}
-
-function setCursor(){
-    const el = document.getElementById('selectable');  
+function setCursor(el){ 
     const selection = window.getSelection();  
     const range = document.createRange();  
     selection.removeAllRanges();  
@@ -156,10 +134,24 @@ function setCursor(){
 }
 
 
+function printTodos(){
+
+    container.innerHTML = ""
+    for (let index = 0; index < todos.length; index++) {
+        
+        container.insertAdjacentHTML('beforeend',taskItem(todos[index]));
+    }
+
+}
+
 /*--------------------------------------------------------*/
 
-const todos = [];
 let container = document.querySelector('[for="target"]')
+const todos = [{
+    done: "",
+    content: "",
+}]
+
 const config = {  
     attributes: true,
     characterData: true,
@@ -169,18 +161,74 @@ const config = {
     characterDataOldValue: true 
 };
 
-todos[0] = {
-    id: 0,
-    done: "",
-    content: "",
-}
+
+printTodos()
+    
+new MutationObserver((mutationsList) => {
+    // console.log(mutationsList.length)
+    
+    mutationsList.forEach(function(mutation) {
+
+        if(document.contains((mutation.target))) {
+            console.log('#child has been added');
+            //observer.disconnect();
+        }
+
+        (mutation.target).addEventListener('keyup', keyup);
+        
+        (mutation.target).addEventListener('click', click)
+    }); 
+
+}).observe(container, config)
 
 
-let cant = 0;
 
 
-showTask2();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let items = document.querySelectorAll(".task")
+/*
+let items = document.querySelectorAll(".task");
+container.addEventListener('DOMNodeInserted', e => {
+
+   
+    
+    items.forEach((item,i) => {
+            
+        item.addEventListener('keyup', enter.bind(e,i))
+        printTodos()
+            
+            // item.removeEventListener('keydown', enter.bind(e,i))
+    })
+    
+    e.preventDefault()
+    // console.log(e.target.childNodes[1])
+    console.log(items.length)
+    console.log(todos);
+    items = document.querySelectorAll(".task")
+    
+}, false);
+ */
 
 
 
